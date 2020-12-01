d1fd4a70fa9ea08ff344ce94a48d52a2ea7bb7fe
Jebati <www.den-safonov@mail.ru>
2020-12-01 06:23:10 +0300

    Update error-messages.enum.ts
    Co-Authored-By: zax2002 <zax@2oo2.ml>

--- src/app/enums/error-messages.enum.ts
+++ src/app/enums/error-messages.enum.ts
@@ -1,4 +1,5 @@
 export enum ErrorMessagesCode {
+  // Common
   NoError = 0,
   InvalidModel = 1,
   EntityNotFound = 3,
@@ -9,25 +10,45 @@ export enum ErrorMessagesCode {
   EmailAlreadyTaken = 102,
 
   // Rooms
-  DomainAlreadyTaken = 200,
-  RoomNotFound = 201,
-  RoomAccessError = 202,
+  RoomNotFound = 200,
+  RoomAccessError = 201,
+
+  DomainAlreadyTaken = 202,
+
   RoomRegistrationEnabled = 203,
 
   // Tests
   TestNotFound = 300,
+  TestAccessError = 301,
+
+  SectionNotFound = 302,
+  QuestionNotFound = 303,
+
+  NotEnoughSections = 304,
+  NotEnoughQuestions = 305,
+
+  SingleChoiceWrongCorrectsCount = 306,
+
+  EmptyOption = 307,
+  EmptyQuestion = 308,
+
+  NoAttemptsLeft = 309,
+
+  TestIsNotActive = 310,
 
   // Users
   UserNotFound = 400,
 
   // Members
   MemberNotFound = 500,
-  MemberAlreadyExists = 501,
-  MemberAccessError = 502,
+  MemberAccessError = 501,
+
+  MemberAlreadyRegistered = 502,
 
   FieldNotPassed = 503,
-  FieldRegexMissmatch = 504,
+  FieldRegexMismatch = 504,
   FieldOptionNotInList = 505,
+
   FieldsAlreadyExists = 506,
 
   MemberIsNotApproved = 507,
@@ -36,28 +57,54 @@ export enum ErrorMessagesCode {
   MemberIsNotRegistered = 509,
   MemberIsRegistered = 510,
 
-  RegistrationDisabled = 511,
-  RegistrationEnabled = 512,
+  HasNoActiveAttempt = 511,
+  HasActiveAttempt = 512,
+
+  // Attempts
+  AttemptNotFound = 600,
+  AttemptAccessError = 601,
+
+  SelectedOptionOutOfRange = 602,
+  WrongOptionsCount = 603,
+  NonUniqueOption = 604,
+  TimeIsUp = 605,
 }
 
 export enum ErrorMessagesText {
   InvalidModel = 'Неверная модель',
   EntityNotFound = 'Сущность не найдена',
-  NothingChanged = 'Ничего не изменилось',
 
   // Auth
-  WrongAuthData = 'Неверные данные аутентификации',
-  LoginAlreadyTaken = 'Логин уже занят',
-  EmailAlreadyTaken = 'Email уже занят',
+  WrongAuthData = 'Неверные данные авторизации',
+  LoginAlreadyTaken = 'Логин занят',
+  EmailAlreadyTaken = 'Email занят',
 
   // Rooms
-  DomainAlreadyTaken = 'Домен уже занят',
   RoomNotFound = 'Комната не найдена',
   RoomAccessError = 'Ошибка доступа к комнате',
+
+  DomainAlreadyTaken = 'Комната с таким доменом уже существует',
+
   RoomRegistrationEnabled = 'Регистрация в комнате включена',
 
   // Tests
   TestNotFound = 'Тест не найден',
+  TestAccessError = 'Ошибка доуступа к тесту',
+
+  SectionNotFound = 'Раздел не найден',
+  QuestionNotFound = 'Вопрос не найден',
+
+  NotEnoughSections = 'Количество выдаваемых секций больше количества секций в вопросе',
+  NotEnoughQuestions = 'Количество выдаваемых вопросов больше количества вопросов в секции',
+
+  SingleChoiceWrongCorrectsCount = 'Неверное количество верных ответов',
+
+  EmptyOption = 'В варианте ответа нет ни текста, ни картинки',
+  EmptyQuestion = 'В вопросе нет ни текста, ни картинки',
+
+  NoAttemptsLeft = 'Попыток не осталось',
+
+  TestIsNotActive = 'Тест не активен',
 
   // Users
   UserNotFound = 'Пользователь не найден',
@@ -67,17 +114,31 @@ export enum ErrorMessagesText {
   MemberAlreadyExists = 'Участник уже существует',
   MemberAccessError = 'Ошибка доступа к участнику',
 
-  FieldNotPassed = 'Не все поля заполнены',
-  FieldRegexMissmatch = 'Несоответствие регулярного выражения поля',
-  FieldOptionNotInList = 'Варианта поля нет в списке',
-  FieldsAlreadyExists = 'Такой участник уже существует',
+  MemberAlreadyRegistered = 'Участник с такими данными полей уже зарегистрирован',
 
-  MemberIsNotApproved = 'Участник не утвержден',
-  MemberIsApproved = 'Участник подвержен ',
+  FieldNotPassed = 'Не все поля заполнены',
+
+  FieldRegexMismatch = 'Введённое значение поля не соответствует маске',
+  FieldOptionNotInList = 'Переданного значения поля нет в списке вариантов',
+
+  FieldsAlreadyExists = 'Участник с такими данными полей уже зарегистрирован',
+
+  MemberIsNotApproved = 'Участник не подтверждён',
+  MemberIsApproved = 'Участник подтверждён',
 
   MemberIsNotRegistered = 'Участник не зарегистрирован',
   MemberIsRegistered = 'Участник зарегистрирован',
 
-  RegistrationDisabled = 'Регистрация выключена',
-  RegistrationEnabled = 'Регистрация включена',
+  HasNoActiveAttempt = 'Нет активной попытке',
+  HasActiveAttempt = 'Уже есть актвная попытка',
+
+  // Attempts
+  AttemptNotFound = 'Попытка не найдена',
+  AttemptAccessError = 'Ошибка доступа к попытке',
+
+  SelectedOptionOutOfRange = 'Переданное количество выбранных вариантов больше количества вариантов в вопросе',
+  WrongOptionsCount = 'Количество переданных элементов последовательности не соответствует количеству элементов последовательности в вопросе',
+  NonUniqueOption = 'Переданы дублирующиеся варианты ответа',
+  
+  TimeIsUp = 'Время прохождения теста истекло',
 }

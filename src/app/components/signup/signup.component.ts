c69ec3f27758166afa69b93c39aa837cc2791b24
Jebati <www.den-safonov@mail.ru>
2020-12-14 03:53:05 +0300

    Update signup.component.ts

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -40,12 +40,12 @@ export class SignUpComponent implements OnInit, OnDestroy {
     ],
     password: [
       '',
-      [Validators.required, Validators.minLength(5), Validators.maxLength(30)],
+      [Validators.required, Validators.minLength(6), Validators.maxLength(128)],
     ],
 
     repeatPassword: [
       '',
-      [Validators.required, Validators.minLength(5), Validators.maxLength(30)],
+      [Validators.required, Validators.minLength(6), Validators.maxLength(128)],
     ],
 
     fields: this.fb.group({}),
